b547f86f90e3981d3fe436fa47bf7085cadff3ff
unknown <alejandro.resk@outlook.com>
2023-04-04 11:49:59 -0300

    back-end

--- Project/ejercicios.js
+++ Project/ejercicios.js
@@ -5,11 +5,5 @@ class Persona {
 
     static especie = 'Humano';
 
-    saludar = () => {
-        console.log(`Hola soy ${this.nombre}`);
-    }
 
-    obtenerEspecie = () => {
-        console.log(`Soy ${Persona.especie}`);
-    }
 }
