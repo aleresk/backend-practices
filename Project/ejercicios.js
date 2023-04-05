62ced7e5bdc486f1051037ae792df36f73f173be
unknown <alejandro.resk@outlook.com>
2023-04-05 00:18:18 -0300

    back-end

--- Project/ejercicios.js
+++ Project/ejercicios.js
@@ -15,9 +15,11 @@ class ProductManager{
             product.id = this.products[this.products.length - 1].id + 1;
         };
 
-        //if(this.products.find(code)){
-            //this.products.push(product);        
-        //}
+        if(this.products.find(code)){
+            this.products.push(product);        
+        }else{
+            console.log("el codigo ya existe");
+        }
 
     }
 
@@ -32,4 +34,7 @@ class ProductManager{
 
 const gestionProductos = new ProductManager();
 gestionProductos.addProduct("Celular", "SmartPhone marca Samsung", 900, "//ruta/foto", 543, 43);
+gestionProductos.addProduct("Laptop", "Portatil marca Lenovo", 2900, "//ruta/foto", 234, 12);
+gestionProductos.addProduct("Tablet", "Tableta marca Apple", 1800, "//ruta/foto", 433, 25);
+gestionProductos.addProduct("Desktop", "PC de escritorio marca DELL", 2700, "//ruta/foto", 213, 32);
 console.log(gestionProductos.getProduct());
