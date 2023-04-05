1a1cb3917231d1988bf6023c6c460a6e412423c3
unknown <alejandro.resk@outlook.com>
2023-04-04 23:18:30 -0300

    back-end

--- Project/ejercicios.js
+++ Project/ejercicios.js
@@ -5,8 +5,20 @@ class ProductManager{
     }
 
     addProduct = (title, description, price, thumbnail, code, stock) => {
+        
         const product = {title, description, price, thumbnail, code, stock};
+        
         }
+
+        if(this.products.length === 0){
+            product.id = 1;
+        }else{
+            product.id = this.products[this.products.length - 1];
+        }
+
+        this.product.push(products);        
+        
+
     }
 
     getProduct = () => {
